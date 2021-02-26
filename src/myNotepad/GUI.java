9105bd90fd742e3a266412d5f02d2920d45623f2
miguelmunozh <36938805+miguelmunozh@users.noreply.github.com>
2021-02-25 22:56:01 -0700

    push to main branch, not to master branch

--- src/myNotepad/GUI.java
+++ src/myNotepad/GUI.java
@@ -3,7 +3,6 @@ package myNotepad;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
